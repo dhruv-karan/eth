0997ed5a7b04143a3c526af4d018644a872f278a
dhruv-karan <dhruvkaran24@gmail.com>
2018-10-24 22:47:31 +0530

    zm,hvcxcbv bx

--- compile.js
+++ compile.js
@@ -6,7 +6,5 @@ const solc = require('solc')
 const inboxPath = path.resolve(__dirname,'contracts','index.sol')
 
 const source = fs.readFileSync(inboxPath,'utf8')
-
-console.log(solc.compile(source,1))
-
-module.exports = solc.compile(source,1).contracts[':Inbox '];
+module.exports = solc.compile(source,1).contracts[':Inbox'];
+// console.log(solc.compile(source,1))
