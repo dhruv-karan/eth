f372a23948a7b71b808f89fa16d4758ac85883f1
dhruv-karan <dhruvkaran24@gmail.com>
2018-10-24 22:48:06 +0530

    new test

---
+++ test/Index.test.js
@@ -0,0 +1,31 @@
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+
+const web3 = new Web3(ganache.provider())
+
+const {interface, bytecode} = require('../compile');
+let accounts;
+let indox;
+
+beforeEach("bj guyfy f",async()=>{
+        accounts = await  web3.eth.getAccounts()
+        inbox = await new web3.eth.Contract(JSON.parse(interface))
+         .deploy({data:bytecode,arguments:['hi there']})
+         .send({from:accounts[0],gas:'1000000'})
+})
+ 
+describe('inbox',()=>{
+    it('deplpoy contracts',()=>{
+        assert.ok(inbox.options.address)
+    })
+    it('contains default message', async ()=>{
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'hi there') 
+    })
+    it('should chnge the data',async()=>{
+        await inbox.methods.setMessage('bye').send({from:accounts[0]})
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye') 
+    })
+})
